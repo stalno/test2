161af71bb97aa5c5e4d647cfce8d6f298da3e154
Artem Danilchik <danilchik@gmail.com>
2025-01-30 23:15:16 +0300

    Some docker

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/hello")
+async def say_hello2(q: str):
+    print(f"q: {q}")
+    return {"message": f"Hello"}
